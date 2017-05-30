f9d0142e18da4fbea8e68505c95b743313cc7e79
Q <dillon.jones125@gmail.com>
2017-05-29 18:56:09 -0700

    Set Up Project
    Set up the splash screen, and added in start scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void loadLevel(string name)
+    {
+        print("Level Load Requested For: " + name);
+        //Application.LoadLevel(name); OLD CODE
+
+        //Use this when managing scenes that go in order.
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(name);
+    }
+    public void quitRequest(string name)
+    {
+        print(name);
+        Application.Quit();
+    }
+    public void loadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/musicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicPlayer : MonoBehaviour {
+    static musicPlayer instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("duplicate musicPlayer destroyed");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
